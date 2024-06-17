7126eaafec7fe7f1d988956929ea69ad4fa24006
SwapnilVG <swapnilgadekar2000@gmail.com>
2024-06-17 22:45:33 +0530

    updated code2

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ import bodyParser from 'body-parser';
 const app = express();
 app.use(cors({
     origin:[process.env.FRONTEND_URL],
-    methods:["POST","GET","PUT","DELETE"],
-    credentials:true 
+    methods:["POST","GET","PUT","DELETE"]
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
